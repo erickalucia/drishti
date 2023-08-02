1266837d77e230805ead57e228b61337de1cdcb2
erickalucia <erickamoreno529@gmail.com>
2023-08-01 18:50:39 -0700

    add show and hide goals button

--- drishti_app/src/App.js
+++ drishti_app/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
+  const [showGoals, setShowGoals] = useState(true)
   const [goals, setGoals] = useState([
     {title: "run deception half", id:1},
     {title: "read all books of personal library", id:2},
     {title: "finish morning journal", id:3}
   ])
+
+  console.log(showGoals)
 
   const handleClick = (id) => {
     setGoals(goals.filter((goal) => {
@@ -26,7 +29,17 @@ function App() {
   return (
     <div className="App">
       <h1>Ericka's Drishti</h1>
-      {goals.map((event, index) => (
+      {showGoals && (
+      <div>
+        <button onClick={() => setShowGoals(false)}> hide goals</button>
+      </div>
+      )}
+      
+      {!showGoals && ( <div>
+        <button onClick={() => setShowGoals(true)}> show goals </button>
+      </div>)}
+      
+      {showGoals && goals.map((event, index) => (
         <div key={event.id}>
           <h2>{index+1} - {event.title}</h2>
           <button onClick={() => handleClick(event.id)}>delete goal</button>
