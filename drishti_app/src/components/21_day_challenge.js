28cebe48fef03a0e73a841190f06e7e5a2bc58e4
erickalucia <erickamoreno529@gmail.com>
2023-08-03 12:51:34 -0700

    add 21 day challenge folder and temporary data storage folder

---
+++ drishti_app/src/components/21_day_challenge.js
@@ -0,0 +1,6 @@
+// ideas
+// ? checked : unchecked 
+// input type checkbox - give each checkbox a value - do a multiselect with the checkboxes (html or js?)
+// state variable: object with each checkbox values 
+// checkbox react component and do loop to render it 21 times but might be easier to hard code the 21 boxes 
+// put state of all checkboxes in one array or something else. 
